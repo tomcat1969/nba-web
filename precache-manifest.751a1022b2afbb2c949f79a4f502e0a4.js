c8d5e7bae300368ed9e1662addba258cc3d1c8ae
HuangLin <tomcat1969@gmail.com>
2019-04-08 21:32:45 -0700

    Updates

---
+++ precache-manifest.751a1022b2afbb2c949f79a4f502e0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27c293f49f124bc98a77bb3feda5952f",
+    "url": "/nba-web/static/media/nba-logoman.27c293f4.svg"
+  },
+  {
+    "revision": "9f89bec480ab44cad98f",
+    "url": "/nba-web/static/js/runtime~main.9f89bec4.js"
+  },
+  {
+    "revision": "748a18382296934687c5",
+    "url": "/nba-web/static/js/main.748a1838.chunk.js"
+  },
+  {
+    "revision": "dd388d79ca852d5f6b50",
+    "url": "/nba-web/static/js/1.dd388d79.chunk.js"
+  },
+  {
+    "revision": "748a18382296934687c5",
+    "url": "/nba-web/static/css/main.77458871.chunk.css"
+  },
+  {
+    "revision": "dd388d79ca852d5f6b50",
+    "url": "/nba-web/static/css/1.94df10a7.chunk.css"
+  },
+  {
+    "revision": "e2a4764449b0b42fe1070f630868f047",
+    "url": "/nba-web/index.html"
+  }
+];
